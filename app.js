95308b40b1861723de09ae94c03ec2756f4bc94d
Manraj Singh <manraj.singh@ucalgary.ca>
2022-08-16 14:41:52 -0600

    Added MongoDB database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
@@ -9,25 +10,51 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var items = [];
-app.get("/", function(req, res){
-    let currentDate = new Date();
-    let option = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
+// Connecting + creating a new DB
+mongoose.connect("mongodb://localhost:27017/listDB");
 
-    let day = currentDate.toLocaleDateString("en-US", option);
-    res.render('design', {dateString: day, itemsArray: items});
+const itemsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please check data entry, you must enter a task name!"]
+    }
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+app.get("/", function(req, res){
+
+    // Finding all items in the Items collection
+    Item.find({}, function(err, itemsReturned){
+
+        // Rendering EJS design on front-end
+        res.render('design', {dateString: "Today", itemsArray: itemsReturned});    
+    })
 
 }); 
 
 app.post("/", function(req, res){
+
+    // Getting input item, saving to database, displaying on front-end
     let inputItem = req.body.newItem;
-    items.push(inputItem);
+    const newItem = new Item({
+        name: inputItem
+    });
+    newItem.save();
     res.redirect("/");
 })
+
+
+app.post("/delete", function(req, res){
+
+    // Getting checked item id, removing it from database, displaying it from front-end
+    const itemId = req.body.checkBox;
+    Item.findByIdAndRemove({_id: itemId}, function(err){
+        if (err) {console.log("Error deleting item.");}
+        else {console.log("Successfully deleted item.");}
+    });
+    res.redirect("/");
+
+});
 
 app.listen(port, function(){
     console.log("Connected to port: " + port);
